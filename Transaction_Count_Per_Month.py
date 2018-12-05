c331ff3ce96792b1e6d0ee5c5e9ee7d00eed3420
Andria Njoku <andrianjoku@gmail.com>
2018-12-05 01:31:22 +0000

    In fully working order now, ready for submission.

--- Transaction_Count_Per_Month.py
+++ Transaction_Count_Per_Month.py
@@ -8,24 +8,26 @@ from mrjob.job import MRJob
 from mrjob.step import MRStep
 import re
 from datetime import datetime
+from mr3px.csvprotocol import CsvProtocol
 
 TRANSACTION_RE = re.compile(",")
 
 
 
-def convertUnixtoDate(timeStamp):
-        
-        #we just need year and month here 
-        
-        dt = datetime.utcfromtimestamp(timeStamp).strftime('%Y-%m')
-        
-        yield(dt)
-
-
-
-
- 
 class TransactionCount(MRJob):
+    
+   
+    
+    #HADOOP_OUTPUT_FORMAT = 'org.apache.hadoop.mapred.SequenceFileOutputFormat'
+    
+    #LIBJARS = ['hadoop.jar']
+    
+    def steps(self):
+        return [
+         MRStep(mapper=self.mapper_get_transactions,
+                combiner=self.combiner_count_transactions,
+                reducer=self.reducer_count_transactions)
+              ]
     
         
  
@@ -35,25 +37,21 @@ class TransactionCount(MRJob):
             # [1] Block ID
             # [2] Date/Time ( in the form of unix seconds malacky)
               
-        
         # yield each word in the line
         transaction = re.split(',', line)
-        
-      #transactionID = transaction[0]
-          
-        timeStamp = int(transaction[2])
-        
+    #transactionID = transaction[0] 
+        timeStamp =float(transaction[2])
         #will return a date object instead of the string timestamp
         
-        dateTimeFinal=convertUnixtoDate(timeStamp)
-        
-        dateTimeFinalString=str(dateTimeFinal)
+        dt = datetime.utcfromtimestamp(timeStamp)
+         
+        stringDate = dt.strftime('%Y-%m') 
         
         #We are simply counting the amount of transactions so we dont really
         #need and value just a key we can use a combinator together with a simple 
         #one to count the amount of transactions for each month
             
-        yield(dateTimeFinalString, 1)
+        yield(stringDate, 1)
         
 
     def combiner_count_transactions(self, YearMonth, counts):
@@ -63,20 +61,17 @@ class TransactionCount(MRJob):
     def reducer_count_transactions(self, YearMonth, counts):
         # send all (num_occurrences, word) pairs to the same reducer.
         # num_occurrences is so we can easily use Python's max() function.
-        yield (YearMonth ,sum(counts))
+        
+        newKey = YearMonth.strip('"/')
+          
+          
+        yield (newKey ,sum(counts))
+        
+ 
 
 
-    def steps(self):
-        return [
-            MRStep(mapper=self.mapper_get_transactions,
-                   combiner=self.combiner_count_transactions,
-            MRStep(reducer=self.reducer_count_transactions))
-           
-        ]
-
+    
 
 if __name__ == '__main__':
     TransactionCount.run()
 
-if __name__ == '__main__':
-    pass
